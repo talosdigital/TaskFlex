a25ad35c01c6b482c81fe5aa3f397b7c42c0c9b2
Santiago Vanegas Gil <savanegasg@gmail.com>
2015-12-30 16:36:46 -0500

    Starting with a new clean gulp file, injecting bower dependencies and custom styles and scripts. Serving using express.

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,34 +1,75 @@
-var gulp      = require('gulp'),
-    connect   = require('gulp-connect'),
-    less      = require('gulp-less'),
-    Server    = require('karma').Server;
-    minifyCss = require('gulp-minify-css');
+var gulp    = require('gulp'),
+    clean   = require('gulp-clean'),
+    connect = require('gulp-connect'),
+    express = require('gulp-express'),
+    inject  = require('gulp-inject'),
+    less    = require('gulp-less'),
+    open    = require('gulp-open'),
+    path    = require('path'),
+    wiredep = require('wiredep').stream;
 
-gulp.task('test', function (done) {
-  new Server({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
-  }, done).start();
-});
+var config = {
+  app: 'app',
+  dist: 'dist',
+  tmp: '.tmp'
+};
 
 gulp.task('less', function () {
-  return gulp.src('./app/assets/less/styles.less')
+  return gulp.src(path.join(config.app, '/assets/less/styles.less'))
     .pipe(less())
-    .pipe(gulp.dest('./app/assets/css'));
+    .pipe(gulp.dest(path.join(config.app, '/assets/css')));
 });
 
-gulp.task('min', function() {
-  return gulp.src('./app/assets/css/styles.css')
-    .pipe(minifyCss({ compatibility: 'ie8' }))
-    .pipe(gulp.dest('dist'));
+gulp.task('inject-vendor', function () {
+  return gulp.src(path.join(config.app, '/index.html'))
+    .pipe(wiredep())
+    .pipe(gulp.dest(config.app));
 });
 
-gulp.task('connect', function() {
+gulp.task('inject-custom', ['less'], function () {
+  var target = gulp.src(path.join(config.app, '/index.html'));
+  var sources = gulp.src([
+    path.join('!' + config.app, '/lib/**/*'),
+    path.join(config.app, '/**/*.js'),
+    path.join(config.app, '/assets/css/**/*.css')
+  ], { read: false });
+  return target.pipe(inject(sources, { relative: true }))
+    .pipe(gulp.dest(config.app));
+});
+
+gulp.task('inject', ['inject-vendor', 'inject-custom']);
+
+
+gulp.task('serve', function () {
+  express.run('app.js');
+});
+
+gulp.task('open', function () {
   var port = process.env.TASKFLEX_FRONT_END_PORT || 8000;
-  connect.server({
-    root: 'app',
-    port: port
-  });
+  var options = {
+    uri: 'http://localhost:' + port,
+  };
+  gulp.src(__filename)
+    .pipe(open(options));
 });
 
-gulp.task('default', ['less', 'connect']);
+gulp.task('clean', function() {
+  return gulp.src([
+    config.dist,
+    config.tmp
+  ], { read: false })
+    .pipe(clean());
+})
+
+// gulp.task('reload', function () {
+//   gulp.src('./app/**/*.*')
+//     .pipe(connect.reload());
+// });
+//
+// gulp.task('watch', function () {
+//   gulp.watch(['./app/**/*.html'], ['reload']);
+//   gulp.watch(['./app/**/*.js'], ['reload']);
+//   gulp.watch(['./app/styles/*.css'], ['reload']);
+// });
+
+gulp.task('default', ['inject', 'serve', 'open']);
