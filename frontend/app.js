5a4907b91c266edea36c5437b51342c3fdd6b7ec
Santiago Vanegas Gil <savanegasg@gmail.com>
2016-01-05 16:58:48 -0500

    Now, gulp tasks generates content ready to be served in a development mode, i.e., without using the dist/ folder

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,17 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.use(express.static('app'));
+var env =  process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  app.use(express.static(path.join(__dirname, '.tmp', 'app')));
+  app.use(express.static(path.join(__dirname, '.tmp', 'partials')));
+  app.use(express.static(path.join(__dirname, 'app')));
+}
+else {
+  app.use(express.static(path.join(__dirname, 'dist')));
+}
 
 var port = process.env.TASKFLEX_FRONT_END_PORT || 8000;
 
